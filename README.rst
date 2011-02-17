726c07d687cd2636ca7a23310cbc96381a545a5e
Reid Ransom <reid@reidransom.com>
2011-02-17 00:50:41 -0800

    encoding fixed for iPhone, uploaded files can have spaces and other wierd characters

--- README.rst
+++ README.rst
@@ -1,15 +1,40 @@
 Goal
 ====
 
-The goal of this project is create a web app that can easily be installed on a 
-computer and allow (primarily) computers on the local network to use it to 
-encode and upload videos for the web.
+The goal of this project is to create a web app that will allow computers 
+(primarily) on the local network to encode and upload videos for the 
+web.
 
 The main problem this project aims to fix is the situation where you have
-multiple content producers needing to push video to the same place or places.
-For example a post production company may have multiple Avid or Final Cut
-editors posting video to the same website for review and approval.
+multiple content producers or editors needing to push video to the same place 
+or places.  For example a post production company may have multiple Avid or 
+Final Cut editors posting video to the same website for review and approval.  
+Rather than configuring export settings for each editing application, 
+installing FTP software, configuring FTP shortcuts, giving out passwords,
+and training people on this process (not to mention how slow these applications
+can be at exporting video for the web), you could have them export same as
+source Quicktimes or Quicktime references and send them a link to a page where
+they can upload the video and let Encloader take care of the rest.
 
 This project should have similar functionality as Apple Compressor except that 
 the user interface should be *much* simpler because the user should only see
-presets which have been created by an "administrator".
+presets which have been created by an Administrator type person.
+
+Requirements
+============
+
+* Python 2.5+ - http://python.org/ (2.5 requires simplejson)
+* Bottle - http://bottle.paws.de/
+* Paste - http://pythonpaste.org/ *Recommended*
+* HandbrakeCLI - http://handbrake.fr/downloads2.php
+
+Install
+=======
+
+Assuming you have Python 2.5+, Bottle and Paste installed, download
+HandbrakeCLI and place the binary in the encloader/handbrake folder.  Then cd
+into the handbrake folder and run::
+
+    python encloadd.py
+
+Then visit http://localhost:8000/ in your web browser to start encloading!
--- settings.py
+++ settings.py
@@ -1,18 +1,42 @@
-PRESETS = {
-    "web_post_full" : {
-        "name" : "SD Web Post",
-        "cmd" : "/usr/bin/HandBrakeCLI -e x264 -q 0.589999973773956 -a 1,1 -E faac,ac3 -B 128,auto -R 48,Auto -6 dpl2,auto --decomb -x level=30:cabac=0:ref=3:mixed-refs=1:analyse=all:me=umh:no-fast-pskip -O -f mp4 -w 640 -l 480 -i %(input_path)s -o %(output_path)s",
-        "host" : "localhost",
-        "user" : "test",
-        "passwd" : "test",
+import os
+
+CWD = os.path.dirname(__file__)
+ENCODE_DIR = os.path.join(CWD, 'uploads')
+DB_FILE = os.path.join(CWD, 'db', 'encload.db')
+HB = os.path.join(CWD, 'bin', 'HandbrakeCLI')
+ENCLOADC = os.path.join(CWD, 'encloadc')
+
+# Before changing or adding your own presets you might find this super useful:
+# http://trac.handbrake.fr/wiki/CLIGuide
+ENCODING_PRESETS = {
+    'web_post_full' : {
+        'name': 'Web Post Full (SD)',
+        'cmd': HB + ' -i %(input_path)s -o %(output_path)s -e x264 -q 20.0 -a 1 -E faac -B 128 -6 dpl2 -R Auto -D 0.0 -f mp4 --width 480 --height 360 --decomb --crop 0:0:0:0 --optimize -m -x cabac=0:ref=2:me=umh:bframes=0:weightp=0:subme=6:8x8dct=0:trellis=0',
+        'extension': '.mp4',
+    },
+    'web_post_wide': {
+        'name': 'Web Post Wide (HD)',
+        'cmd': HB + ' -i %(input_path)s -o %(output_path)s -e x264 -q 20.0 -a 1 -E faac -B 128 -6 dpl2 -R Auto -D 0.0 -f mp4 --width 480 --height 270 --decomb --crop 0:0:0:0 --optimize -m -x cabac=0:ref=2:me=umh:bframes=0:weightp=0:subme=6:8x8dct=0:trellis=0',
+        'extension': '.mp4',
+    },
+}
+
+# Add your ftp site here
+DESTINATION_PRESETS = {
+    'ftp_site': {
+        'name': 'Example FTP',
+        'host': 'ftp.example.com',
+        'user': 'username',
+        'passwd': 'password',
         #"port" : 21,
         #"path" : "/",
     },
-    "web_post_wide" : {
-        "name" : "SD Web Post",
-        "cmd" : "/usr/bin/HandBrakeCLI -e x264 -q 0.589999973773956 -a 1,1 -E faac,ac3 -B 128,auto -R 48,Auto -6 dpl2,auto --decomb -x level=30:cabac=0:ref=3:mixed-refs=1:analyse=all:me=umh:no-fast-pskip -O -f mp4 -w 768 -l 432 -i %(input_path)s -o %(output_path)s",
-        "host" : "localhost",
-        "user" : "test",
-        "passwd" : "test",
+    'b2': {
+        'name': 'B2 User',
+        'host': 'b2-studios.com',
+        'user': 'b2user@b2-studios.com',
+        'passwd': 'w00leyb2',
+        #"port" : 21,
+        #"path" : "/",
     },
 }
