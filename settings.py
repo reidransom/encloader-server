c9a92295414e1bbea0f7baa15229e49db980e342
Reid Ransom <reid@reidransom.com>
2011-07-05 14:11:07 -0700

    increased video width and height for SD and HD presets

--- settings.py
+++ settings.py
@@ -11,12 +11,12 @@ ENCLOADC = os.path.join(CWD, 'encloadc')
 ENCODING_PRESETS = {
     'web_post_full' : {
         'name': 'Web Post Full (SD)',
-        'cmd': HB + ' -i %(input_path)s -o %(output_path)s -e x264 -q 20.0 -E faac -B 128 -6 dpl2 -R Auto -D 0.0 -f mp4 --width 480 --height 360 --decomb --crop 0:0:0:0 --optimize -m -x cabac=0:ref=2:me=umh:bframes=0:weightp=0:subme=6:8x8dct=0:trellis=0',
+        'cmd': HB + ' -i %(input_path)s -o %(output_path)s -e x264 -q 20.0 -E faac -B 128 -6 dpl2 -R Auto -D 0.0 -f mp4 --width 640 --height 480 --decomb --crop 0:0:0:0 --optimize -m -x cabac=0:ref=2:me=umh:bframes=0:weightp=0:subme=6:8x8dct=0:trellis=0',
         'extension': '.mp4',
     },
     'web_post_wide': {
         'name': 'Web Post Wide (HD)',
-        'cmd': HB + ' -i %(input_path)s -o %(output_path)s -e x264 -q 20.0 -E faac -B 128 -6 dpl2 -R Auto -D 0.0 -f mp4 --width 480 --height 270 --decomb --crop 0:0:0:0 --optimize -m -x cabac=0:ref=2:me=umh:bframes=0:weightp=0:subme=6:8x8dct=0:trellis=0',
+        'cmd': HB + ' -i %(input_path)s -o %(output_path)s -e x264 -q 20.0 -E faac -B 128 -6 dpl2 -R Auto -D 0.0 -f mp4 --width 960 --height 540 --decomb --crop 0:0:0:0 --optimize -m -x cabac=0:ref=2:me=umh:bframes=0:weightp=0:subme=6:8x8dct=0:trellis=0',
         'extension': '.mp4',
     },
 }
